d16aef3df371cdc51e251aceac4f1ed816fdd7cb
florian666 <florian.mosnier@laposte.net>
2014-12-19 19:13:53 +0100

    Update SPRITE.h

--- Mario/SPRITE.h
+++ Mario/SPRITE.h
@@ -1,13 +1,8 @@
 #ifndef _SPRITE_H
     #define _SPRITE_H
 	
-	extern const unsigned char tile_01[]; // tile carré
-	extern const unsigned char tile_02[]; // tile point d'interrogation
-	extern const unsigned char tile_03[]; // tile fin tuyeau
-	extern const unsigned char tile_04[]; // tile début tuyeau
-	extern const unsigned char tile_05[]; // tile brique cassable
-	extern const unsigned char tile_06[]; // tile grosse piece
-	extern const unsigned char tile_07[]; // tile piece normale
+	extern const unsigned char tile[7][8];
+	
 	extern const unsigned char Mario_droite1[];
 	extern const unsigned char Mario_droite2[];
 	extern const unsigned char Mario_gauche1[];
